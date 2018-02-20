92935e09048bfbc6260c1fe50f45464f916223cf
thdaemon <thxdaemon@gmail.com>
2018-02-20 18:33:12 +0800

    Add menuhelper

--- main.c
+++ main.c
@@ -19,6 +19,7 @@
 #include "paths.h"
 #include "srvwin.h"
 #include "conf.h"
+#include "menuhelper.h"
 
 static GtkWidget *defserver_menu;
 
@@ -51,69 +52,6 @@ static void mevt_sys_proxy(GtkMenuItem *menuitem, gpointer user_data)
 {
 	sys_proxy = (int)gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(menuitem));
 	need_update = 1;
-}
-
-static GtkWidget *add_menu_item(GtkWidget *menu, gchar *lable, gpointer callback)
-{
-	GtkWidget *item;
-
-	item = gtk_menu_item_new_with_label(lable);
-
-	if (callback)
-		g_signal_connect(item, "activate", G_CALLBACK(callback), item);
-
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
-	gtk_widget_show(item);
-
-	return item;
-}
-
-static GtkWidget *add_menu_check_item(GtkWidget *menu, gchar *lable, gpointer callback,
-				      gboolean is_active)
-{
-	GtkWidget *item;
-
-	item = gtk_check_menu_item_new_with_label(lable);
-
-	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(item), is_active);
-
-	if (callback)
-		g_signal_connect(item, "activate", G_CALLBACK(callback), item);
-
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
-	gtk_widget_show(item);
-
-	return item;
-}
-
-static GtkWidget *add_menu_radio_item(GtkWidget *menu, GSList **group, gchar *lable,
-			       gpointer callback, gboolean is_active, gpointer udata)
-{
-	GtkWidget *item;
-
-	item = gtk_radio_menu_item_new_with_label(*group, lable);
-
-	if (*group == NULL) {
-		GSList *g = gtk_radio_menu_item_get_group(GTK_RADIO_MENU_ITEM(item));
-		*group = g;
-	}
-
-	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(item), is_active);
-
-	if (callback)
-		g_signal_connect(item, "activate", G_CALLBACK(callback), udata);
-
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
-	gtk_widget_show(item);
-
-	return item;
-}
-
-static void add_menu_separator(GtkWidget *menu)
-{
-	GtkWidget *item;
-	item = gtk_separator_menu_item_new();
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
 }
 
 void update_defserver_menu_list()
